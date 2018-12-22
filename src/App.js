a6c04f30118b331556ccb987cc325d59fcce54d5
Boris Civcic <civcicboris93@gmail.com>
2018-12-22 22:17:31 +0100

    add: steps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
-import { BrowserRouter } from 'react-router-dom'
-import Test from './containers/test'
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './containers/home'
+import Header from './components/header'
+import stepOne from './containers/steps/stepOne'
+
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <BrowserRouter>
           <Switch>
-            <Route exact path='/' component={() => (<div>home</div>)} />
-            <Route path='/test' component={Test} />
+            <Route path='/step-1' component={stepOne} />
+            <Route exact path='/' component={Home} />
           </Switch>
         </BrowserRouter>
       </div>
